9d14e091f40a8a6ee17f69636c818498de293dcd
kartik-v <kartikv2@gmail.com>
2013-12-17 11:31:46 +0530

    Update StrengthValidatorAsset.php

--- password/StrengthValidatorAsset.php
+++ password/StrengthValidatorAsset.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace kartik\password;
 
 use yii\web\AssetBundle;
@@ -6,13 +7,14 @@ use yii\web\AssetBundle;
 /**
  * @author Kartik Visweswaran <kartikv2@gmail.com>
  */
-class StrengthValidatorAsset extends AssetBundle
-{
-	public $sourcePath = '@vendor/kartik-v/yii2-password/kartik/assets';
-	public $js = [
-		'js/strength-validation.js',
-	];
-	public $depends = [
-		'yii\web\JqueryAsset'
-	];
+class StrengthValidatorAsset extends AssetBundle {
+
+    public $sourcePath = '@vendor/kartik-v/yii2-password/kartik/assets';
+    public $js = [
+        'js/strength-validation.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset'
+    ];
+
 }
