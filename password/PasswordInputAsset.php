8bb18310a701f76ff51011bb9188017ef11f93d9
kartik-v <kartikv2@gmail.com>
2013-12-17 11:30:43 +0530

    Update PasswordInputAsset.php

--- password/PasswordInputAsset.php
+++ password/PasswordInputAsset.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace kartik\password;
 
 use yii\web\AssetBundle;
@@ -6,17 +7,18 @@ use yii\web\AssetBundle;
 /**
  * @author Kartik Visweswaran <kartikv2@gmail.com>
  */
-class PasswordInputAsset extends AssetBundle
-{
-	public $sourcePath = '@vendor/kartik-v/yii2-password/kartik/assets';
-	public $css = [
-		'css/strength-meter.css',
-	];
-	public $js = [
-		'js/strength-meter.js',
-	];
-	public $depends = [
-		'yii\web\JqueryAsset',
-		'yii\bootstrap\BootstrapAsset',
-	];
+class PasswordInputAsset extends AssetBundle {
+
+    public $sourcePath = '@vendor/kartik-v/yii2-password/kartik/assets';
+    public $css = [
+        'css/strength-meter.css',
+    ];
+    public $js = [
+        'js/strength-meter.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+
 }
