9a0fde9015a2f864fe37318d420eca4483af30b9
kartik-v <kartikv2@gmail.com>
2013-12-31 17:46:24 +0400

    Minification of assets based on YII_DEBUG

--- password/PasswordInputAsset.php
+++ password/PasswordInputAsset.php
@@ -5,20 +5,23 @@ namespace kartik\password;
 use yii\web\AssetBundle;
 
 /**
+ * Asset bundle for PasswordInput Widget
+ *
  * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
  */
 class PasswordInputAsset extends AssetBundle {
 
     public $sourcePath = '@vendor/kartik-v/yii2-password/kartik/assets';
-    public $css = [
-        'css/strength-meter.css',
-    ];
-    public $js = [
-        'js/strength-meter.js',
-    ];
     public $depends = [
         'yii\web\JqueryAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
 
+	public function init() {
+		$this->css = YII_DEBUG ? ['css/strength-meter.css'] : ['css/strength-meter.min.css'];
+		$this->js = YII_DEBUG ? ['js/strength-meter.js'] : ['js/strength-meter.min.js'];
+		parent::init();
+	}
+	
 }
--- password/StrengthValidatorAsset.php
+++ password/StrengthValidatorAsset.php
@@ -4,17 +4,20 @@ namespace kartik\password;
 
 use yii\web\AssetBundle;
 
+
 /**
+ * Asset bundle for StrengthValidator
+ *
  * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
  */
 class StrengthValidatorAsset extends AssetBundle {
-
     public $sourcePath = '@vendor/kartik-v/yii2-password/kartik/assets';
-    public $js = [
-        'js/strength-validation.js',
-    ];
     public $depends = [
         'yii\web\JqueryAsset'
     ];
-
+	public function init() {
+		$this->js = YII_DEBUG ? ['js/strength-validation.js'] : ['js/strength-validation.min.js'];
+		parent::init();
+	}
 }
