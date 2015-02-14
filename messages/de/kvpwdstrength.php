7a89c70c4c91bdb93fbf08d5789281ae16c4e2dd
Evil Karter <evil.karter@gmail.com>
2015-02-14 22:08:45 +0100

    remove copy paste error

--- messages/de/kvpwdstrength.php
+++ messages/de/kvpwdstrength.php
@@ -17,25 +17,6 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-   <?php
-
-/**
- * Message translations for \kartik\password\StrengthValidator.
- *
- * It contains the localizable messages extracted from source code.
- * You may modify this file by translating the extracted messages.
- *
- * Each array element represents the translation (value) of a message (key).
- * If the value is empty, the message is considered as not translated.
- * Messages that no longer need translation will have their translations
- * enclosed between a pair of '@@' marks.
- *
- * Message string can be used with plural forms format. Check i18n section
- * of the guide for details.
- *
- * NOTE: this file must be saved in UTF-8 encoding.
- */
-return [
     '{attribute} should contain at least {n, plural, one{one character} other{# characters}} ({found} found)!' =>
     '{attribute} muss mindestens {n, plural, one{ein Zeichen} other{# Zeichen}} enthalten ({found} gefunden)!',
     '{attribute} should contain at most {n, plural, one{one character} other{# characters}} ({found} found)!' =>
@@ -53,5 +34,4 @@ return [
         '{attribute} muss mindestens {n, plural, one{eine Ziffer enthalten} other{# Ziffern enthalten}} ({found} gefunden)!',
     '{attribute} should contain at least {n, plural, one{one special character} other{# special characters}} ({found} found)!' =>
         '{attribute} muss mindestens {n, plural, one{ein Sonderzeichen enthalten} other{# Sonderzeichen enthalten}} ({found} gefunden)!',
-];
 ];
